bf40154f50db933b6a96432a9ec88fdc53f48d4f
Andrew Nguyen <andrewn@conceptsoftworks.com>
2023-07-22 01:52:05 +0000

    Fix/microservice schema

--- src/validations/battle.ts
+++ src/validations/battle.ts
@@ -1,28 +1,5 @@
 import { z } from 'zod';
 
-const characterSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-	stats: z.array(
-		z.object({
-			id: z.number(),
-			value: z.number(),
-		})
-	),
-	equipment: z.array(
-		z.object({
-			name: z.string(),
-			bonuses: z.array(
-				z.object({
-					statId: z.number(),
-					value: z.number(),
-				})
-			),
-		})
-	),
-	action: z.string(),
-});
-
 const mobSchema = z.object({
 	id: z.number(),
 	name: z.string(),
@@ -44,12 +21,38 @@ const bonusSchema = z.object({
 });
 
 const statSchema = z.object({
-	id: z.number(),
-	value: z.number(),
+	StatId: z.number(),
+	Experience: z.number(),
 });
 
+const itemSchema = z.object({
+	Name: z.string().nullable(),
+	Description: z.string(),
+	ItemTypeId: z.number(),
+	Id: z.number(),
+	Type: z.string(),
+	Stats: z.array(statSchema)
+})
+
+
+const equipmentSchema = z.object({
+	ItemId: z.number(),
+	Durability: z.number().nullable(),
+	Item: itemSchema
+})
+
+
+const characterSchema = z.object({
+	id: z.number(),
+	name: z.string(),
+	stats: z.array(statSchema),
+	equipment: z.array(equipmentSchema),
+	action: z.string(),
+});
+
+
 export const battleSchema = z.object({
-	definedStats: z.array(z.object({ id: z.number(), name: z.string() })),
+	definedStats: z.array(z.object({ id: z.number(), name: z.string(), levelScale: z.number() })),
 	character: characterSchema,
 	mob: mobSchema,
 	bonuses: z.array(bonusSchema),
@@ -59,3 +62,4 @@ export type Character = z.infer<typeof characterSchema>;
 export type Mob = z.infer<typeof mobSchema>;
 export type Bonus = z.infer<typeof bonusSchema>;
 export type Stat = z.infer<typeof statSchema>;
+export type Equipment = z.infer<typeof equipmentSchema>
