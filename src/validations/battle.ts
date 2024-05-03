0b44e1e58b7e0891e97410fbf2befbe62105198a
Andrew Nguyen <andyt.nguyen123@gmail.com>
2024-05-02 21:49:53 -0700

    fix(validations/battle): add effects to schema; reconfigure other schemas

--- src/validations/battle.ts
+++ src/validations/battle.ts
@@ -1,66 +1,81 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const mobSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-	monster: z.object({
-		lore: z.string(),
-		legion: z.string(),
-		gasLighting: z.string(),
-		parameters: z.string(),
-		heart: z.string(),
-		personality: z.string(),
-	}),
-	difficulty: z.number(),
+  id: z.number(),
+  name: z.string(),
+  monster: z.object({
+    lore: z.string(),
+    legion: z.string(),
+    gasLighting: z.string(),
+    parameters: z.string(),
+    heart: z.string(),
+    personality: z.string(),
+  }),
+  difficulty: z.number(),
 });
 
 const bonusSchema = z.object({
-	statId: z.number(),
-	min: z.number(),
-	max: z.number(),
-	bonus: z.number(),
+  statId: z.number(),
+  min: z.number(),
+  max: z.number(),
+  bonus: z.number(),
 });
 
-const statSchema = z.object({
-	StatId: z.number(),
-	Experience: z.number(),
+const skillSchema = z.object({
+  Skill: z.number(),
+  Experience: z.number(),
 });
 
 const itemSchema = z.object({
-	Name: z.string().nullable(),
-	Description: z.string(),
-	ItemTypeId: z.number(),
-	Id: z.number(),
-	Type: z.string(),
-	Stats: z.array(statSchema),
+  Name: z.string().nullable(),
+  Description: z.string(),
+  MaxDurability: z.number(),
 });
 
 const equipmentSchema = z.object({
-	ItemId: z.number(),
-	Durability: z.number().nullable(),
-	Item: itemSchema,
+  Slot: z.number().nullable(),
+  Quantity: z.number().nullable(),
+  Durability: z.number().nullable(),
+  item: itemSchema,
+  effects: z.array(
+    z.object({
+      Value: z.number(),
+      AlwaysApply: z.boolean(),
+      ApplyChance: z.number(),
+      type: z.object({
+        Name: z.string(),
+        Description: z.string(),
+        Key: z.string(),
+        Passive: z.boolean(),
+      }),
+    })
+  ),
 });
 
 const characterSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-	stats: z.array(statSchema),
-	equipment: z.array(equipmentSchema),
+  id: z.number(),
+  name: z.string(),
+  skills: z.array(skillSchema),
+  equipment: z.array(equipmentSchema),
 });
 
 export const battleSchema = z.object({
-	definedStats: z.array(
-		z.object({ id: z.number(), name: z.string(), levelScale: z.number() })
-	),
-	character: characterSchema,
-	mob: mobSchema,
-	monsterAttack: z.boolean(),
-	bonuses: z.array(bonusSchema),
-	action: z.optional(z.string().default('')).nullable(),
+  definedStats: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      levelScale: z.number(),
+    })
+  ),
+  character: characterSchema,
+  mob: mobSchema,
+  monsterAttack: z.boolean(),
+  bonuses: z.array(bonusSchema),
+  action: z.optional(z.string().default("")).nullable(),
 });
 
 export type Character = z.infer<typeof characterSchema>;
 export type Mob = z.infer<typeof mobSchema>;
 export type Bonus = z.infer<typeof bonusSchema>;
-export type Stat = z.infer<typeof statSchema>;
+export type Skill = z.infer<typeof skillSchema>;
 export type Equipment = z.infer<typeof equipmentSchema>;
