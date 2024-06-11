8d18d0b724fe92c2ccaa26792e6d69e4987bf68e
Andrew Nguyen <andyt.nguyen123@gmail.com>
2024-06-11 16:33:01 -0700

    refactor(battle): remove code relating to stats and dicerolling

--- src/controllers/battle.ts
+++ src/controllers/battle.ts
@@ -1,93 +1,33 @@
 import { ChatCompletionRequestMessage } from "openai";
 import { type Request, type Response } from "express";
-import { battleSchema, type Skill } from "@/validations/battle";
+import { battleSchema } from "@/validations/battle";
 import { ZodError } from "zod";
-import {
-  battleOut,
-  calcStats,
-  generateAIResponse,
-  getBonusRoll,
-  getClassification,
-} from "@/helpers/game-mechanics";
+import { generateAIResponse } from "@/helpers/game-mechanics";
 
 export async function battle(req: Request, res: Response) {
   try {
-    const { character, mob, bonuses, definedStats, monsterAttack, action } =
-      battleSchema.parse(req.body);
-
-    /* Fetch user input classification */
-    const { classification } = monsterAttack
-      ? { classification: null }
-      : await getClassification(action || "", definedStats);
-
-    /* Extract effects from equipment */
-    const equipmentEffects = character.equipment
-      .map((equipment) => {
-        const hey = equipment.effects.map(({ value, type }) => ({
-          value,
-          key: type.key,
-        }));
-        return hey;
-      })
-      .flat();
-
-    /* 
-			if a player has 10 Strength, and they use a strength attack, they should get the roll bonus based on their 10 Str (if such a bonus exists)
-		 	Calc total stats of the user and the equipment
-		*/
-    const totalStats: Skill[] = calcStats(character.skills);
-
-    var statUsed: Skill | undefined = undefined;
-    if (classification !== null) {
-      statUsed = totalStats.find(({ skill }) => skill === classification.id);
-    }
-
-    const foundStat = definedStats.find(({ id }) => id === classification?.id);
-
-    /* Check if user has bonus roll */
-    let bonusRoll: number = statUsed
-      ? getBonusRoll(
-          classification?.id,
-          statUsed?.experience,
-          foundStat?.levelScale ?? 0,
-          bonuses
-        )
-      : 0;
-
-    equipmentEffects.forEach(({ key, value }) => {
-      if (classification?.name === key) {
-        bonusRoll += value;
-      }
-    });
-
-    bonusRoll += 1;
-
-    // FIGHT!!!!
-    const battleOutcome = battleOut(bonusRoll, mob.difficulty);
+    const { character, mob } = battleSchema.parse(req.body);
 
     /** Monster response  */
-    if (monsterAttack) {
-      const { monster } = mob;
-      const monstMsgs: ChatCompletionRequestMessage[] = Object.values(
-        monster
-      ).map((content) => ({ role: "system", content }));
+    const { monster } = mob;
+    const monsterMessages: ChatCompletionRequestMessage[] = Object.values(
+      monster
+    ).map((content) => ({ role: "system", content }));
 
-      const aiResponse = await generateAIResponse(monstMsgs, character.name);
-      return res.status(200).json({
-        damage: battleOutcome.monsterDamage,
-        roll: battleOutcome.monsterRoll,
-        response: aiResponse.content,
+    if (character.action) {
+      monsterMessages.push({
+        role: "system",
+        content: `user does this action to you ${character.action}`,
       });
     }
 
-    /** User Response */
+    const aiResponse = await generateAIResponse(
+      monsterMessages,
+      character.name
+    );
     return res.status(200).json({
-      damage: battleOutcome.playerDamage,
-      roll: battleOutcome.playerRoll,
-      classification,
+      response: aiResponse.content,
     });
-
-    // Return classifciation & dice roll
   } catch (error) {
     console.log("ERROR", error);
     if (error instanceof ZodError) {
