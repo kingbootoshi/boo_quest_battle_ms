5fd66315ee0d634d249602beab96037287fa7b8e
Andrew Nguyen <andyt.nguyen123@gmail.com>
2024-06-11 16:31:10 -0700

    refactor(validation/battle): remove unused schemas

--- src/validations/battle.ts
+++ src/validations/battle.ts
@@ -14,68 +14,16 @@ const mobSchema = z.object({
   difficulty: z.number(),
 });
 
-const bonusSchema = z.object({
-  statId: z.number(),
-  min: z.number(),
-  max: z.number(),
-  bonus: z.number(),
-});
-
-const skillSchema = z.object({
-  skill: z.number(),
-  experience: z.number(),
-});
-
-const itemSchema = z.object({
-  name: z.string().nullable(),
-  description: z.string(),
-  maxDurability: z.number(),
-});
-
-const equipmentSchema = z.object({
-  slot: z.number().nullable(),
-  quantity: z.number().nullable(),
-  durability: z.number().nullable(),
-  item: itemSchema,
-  effects: z.array(
-    z.object({
-      value: z.number(),
-      alwaysApply: z.boolean(),
-      applyChance: z.number(),
-      type: z.object({
-        name: z.string(),
-        description: z.string(),
-        key: z.string(),
-        passive: z.boolean(),
-      }),
-    })
-  ),
-});
-
 const characterSchema = z.object({
   id: z.number(),
   name: z.string(),
-  skills: z.array(skillSchema),
-  equipment: z.array(equipmentSchema),
+  action: z.string().nullable(),
 });
 
 export const battleSchema = z.object({
-  definedStats: z.array(
-    z.object({
-      id: z.number(),
-      name: z.string(),
-      levelScale: z.number(),
-    })
-  ),
   character: characterSchema,
   mob: mobSchema,
-  monsterAttack: z.boolean(),
-  bonuses: z.array(bonusSchema),
-  action: z.optional(z.string().default("")).nullable(),
 });
 
 export type Character = z.infer<typeof characterSchema>;
 export type Mob = z.infer<typeof mobSchema>;
-export type Bonus = z.infer<typeof bonusSchema>;
-export type Skill = z.infer<typeof skillSchema>;
-export type Equipment = z.infer<typeof equipmentSchema>;
